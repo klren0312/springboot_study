63ebb54b69848b045b1e1c1c2c4e954e19a2433e
zzes <kuanglong0312@sina.com>
2017-07-29 12:55:05 +0800

    springboot websocket学习

---
+++ websocketdemo/src/main/java/com/klren/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.klren;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Created by zzes on 2017/7/29.
+ */
+
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
+
